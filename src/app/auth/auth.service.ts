2adb049c9f0499ee2bf9dc7177f0d231050a810f
Tomasz Gnyp <tomasz.gnyp4@gmail.com>
2021-03-11 23:53:43 +0100

    Delete API Keys

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -26,7 +26,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyC9faUWZor6yQHt7w5Fkj38Yhltvwjn3Vo',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=[API_KEY]',
         {
           email: email,
           password: password,
@@ -49,7 +49,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyC9faUWZor6yQHt7w5Fkj38Yhltvwjn3Vo',
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=[API_KEY]',
         {
           email: email,
           password: password,
